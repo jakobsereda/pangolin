7a857e0a7b4b4c8242613a24d614368d99b700b4
jakob <138621739+jakobsereda@users.noreply.github.com>
2024-11-12 23:08:40 -0800

    feat: added spec in main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+ *  - read in a file and spits its contents out
+ *     - dont cause a mem leak/buffer overflow
+ *  - parse cmd line args
+ *     - debug flag
+ *     - output file flag 
+ *     - make sure they didnt pass in extra stuff
+ *  - tokenizer/lexer 
+ *  - symbol table
+ *  - parser (recursive descent parser on an absract syntax tree)
+ *  - code gen
+ */
+
 int main(int argc, char *argv[]) {
     printf("erm\n");
 }
