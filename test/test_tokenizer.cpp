051ebdbb6c621d71ea03de0d3bf54c9748a2bada
Ryan Liu <ryanl6699@gmail.com>
2025-06-23 13:15:30 -0700

    unit-tests: ifs and math ops

--- test/test_tokenizer.cpp
+++ test/test_tokenizer.cpp
@@ -11,3 +11,28 @@ TEST_CASE("comparison ops test")
     CHECK(tokens[3].type == TokenType::LessEqual);
     CHECK(tokens[4].type == TokenType::GreaterEqual);
 }
+
+TEST_CASE("math ops and binary ops test")
+{
+    Tokenizer tokenizer("+-/* = % & ^ | ~");
+	std::vector<Token> tokens = tokenizer.tokenize();
+    CHECK(tokens[0].type == TokenType::Plus);
+    CHECK(tokens[1].type == TokenType::Minus);
+    CHECK(tokens[2].type == TokenType::Divide);
+    CHECK(tokens[3].type == TokenType::Multiply);
+    CHECK(tokens[4].type == TokenType::Equals);
+    CHECK(tokens[5].type == TokenType::Modulus);
+    CHECK(tokens[6].type == TokenType::BAnd);
+    CHECK(tokens[7].type == TokenType::BXor);
+    CHECK(tokens[8].type == TokenType::BOr);
+    CHECK(tokens[9].type == TokenType::BNot);
+}
+
+TEST_CASE("if statements test")
+{
+    Tokenizer tokenizer("if (a == 4) {} else if (a == 5) {} else if (a == 9) else {}");
+	std::vector<Token> tokens = tokenizer.tokenize();
+    CHECK(tokens[0].type == TokenType::If);
+    CHECK(tokens[8].type == TokenType::Else);
+    CHECK(tokens[9].type == TokenType::If);
+}
